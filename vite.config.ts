26fe77c08c0f3a55d0db643b47d93373ffa7addd
STnoui <deyan.todorov21@gmail.com>
2025-12-11 09:08:17 +0200

    Optimize build: Split chunks for maplibre, turf, and vendor

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'framer-motion'],
+          maplibre: ['maplibre-gl', '@mapbox/mapbox-gl-draw'],
+          turf: ['@turf/turf'],
+          flexsearch: ['flexsearch'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
 })
